4375a417eec251c6f58f1d72de357e2929ef527a
jooo0922 <2014102458@khu.ac.kr>
2022-06-05 17:49:49 +0900

    refactor: 변수 인라인하기

--- ch1/app.js
+++ ch1/app.js
@@ -5,40 +5,40 @@ const fs = require("fs");
 const invoice = JSON.parse(fs.readFileSync("./invoices.json", "utf8"));
 const plays = JSON.parse(fs.readFileSync("./plays.json", "utf8"));
 
-// 함수 추출하기
-// 새 함수 안에서 값을 변경하지 않는 변수는 매개변수로 전달
-// 자바스크립트같은 동적타입언어는 변수명을 지정할 때 타입이 드러나도록 하는 게 좋음. (하나의 performance 객체라는 것을 표현하기 위해 부정관사 a 를 접두어로 붙임.)
-function amountFor(aPerformance, play) {
-  let result = 0; // 변수를 명확한 이름으로 변경함.
-
-  switch (play.type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+function statement(invoice, plays) {
+  // play 는 perf(aPerformance) 안에서 받아오는 값이라 애초에 매개변수로 넘겨줄 필요가 없었음.
+  // 이런 임시변수들은 질의함수로 바꿔줌.
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 
-  return result; // 함수의 반환 값은 가급적 'result' 라는 이름으로 변수명을 지을 것.
-}
+  // 함수 추출하기
+  // 새 함수 안에서 값을 변경하지 않는 변수는 매개변수로 전달
+  // 자바스크립트같은 동적타입언어는 변수명을 지정할 때 타입이 드러나도록 하는 게 좋음. (하나의 performance 객체라는 것을 표현하기 위해 부정관사 a 를 접두어로 붙임.)
+  function amountFor(aPerformance, play) {
+    let result = 0; // 변수를 명확한 이름으로 변경함.
 
-// play 는 perf(aPerformance) 안에서 받아오는 값이라 애초에 매개변수로 넘겨줄 필요가 없었음.
-// 이런 임시변수들은 질의함수로 바꿔줌.
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
+    switch (play.type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
+    }
 
-function statement(invoice, plays) {
+    return result; // 함수의 반환 값은 가급적 'result' 라는 이름으로 변수명을 지을 것.
+  }
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -49,16 +49,17 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    const play = playFor(perf); // 우변을 함수로 추출
-    let thisAmount = amountFor(perf, play); // 추출한 함수 이용
+    let thisAmount = amountFor(perf, playFor(perf)); // 추출한 함수 이용, 변수 인라인
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5); // 변수 인라인
 
     // 청구 내역을 출력한다.
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${
+    // 변수 인라인
+    result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${
       perf.audience
     }석)\n`;
     totalAmount += thisAmount;
