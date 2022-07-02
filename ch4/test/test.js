3998f6a25f02d5617ad2c2155fdbdba4e1c7420c
jooo0922 <2014102458@khu.ac.kr>
2022-07-03 03:44:16 +0900

    add: expect() 함수로 검증

--- ch4/test/test.js
+++ ch4/test/test.js
@@ -3,12 +3,13 @@
  * import 키워드를 통해 node.js 에서 기본적으로 제공하는 assert 모듈과
  * 테스트를 원하는 코드를 가져올 수 있음.
  */
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import { Province, sampleProvinceData } from "../Province.js";
 
 describe("province", function () {
   it("shortfall", function () {
     const asia = new Province(sampleProvinceData()); // 픽스처(테스트에 필요한 데이터와 객체) 설정
-    assert.equal(asia.shortfall, 5); // 검증
+    // assert.equal(asia.shortfall, 5); // 검증
+    expect(asia.shortfall).equal(5); // expect() 함수로 검증
   });
 });
