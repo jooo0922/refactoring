f4bc57e470787330987399dda4c71290b41cae33
jooo0922 <2014102458@khu.ac.kr>
2022-07-04 00:04:41 +0900

    test: 총 수익 계산로직 단위테스트 추가

--- ch4/test/test.js
+++ ch4/test/test.js
@@ -7,9 +7,16 @@ import { assert, expect } from "chai";
 import { Province, sampleProvinceData } from "../Province.js";
 
 describe("province", function () {
+  // 생산 부족분 계산로직 단위테스트 작성
   it("shortfall", function () {
     const asia = new Province(sampleProvinceData()); // 픽스처(테스트에 필요한 데이터와 객체) 설정
     // assert.equal(asia.shortfall, 5); // 검증
     expect(asia.shortfall).equal(5); // expect() 함수로 검증
+  });
+
+  // 총 수익 계산로직 단위테스트 설정
+  it("profit", function () {
+    const asia = new Province(sampleProvinceData());
+    expect(asia.profit).equal(230);
   });
 });
