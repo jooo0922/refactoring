9cdc31de7bcfda0d2547d5c176f790df3015d420
jooo0922 <2014102458@khu.ac.kr>
2022-07-30 22:37:39 +0900

    refactor: 클래스 문맥에서 표현식을 메서드로 추출

--- ch6/6-3/app2.js
+++ ch6/6-3/app2.js
@@ -12,10 +12,20 @@ class Order {
   }
 
   get price() {
-    return (
-      this.quantity * this.itemPrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
-    );
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+
+  // price() 내에서만 사용할 게 아니라, 클래스처럼 넓은 문맥에서도 사용할 거라면,
+  // 변수가 아닌 메서드(특히, getter)로 추출하는 편이 낫다!
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
+
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+
+  get basePrice() {
+    return this.quantity * this.itemPrice;
   }
 }
