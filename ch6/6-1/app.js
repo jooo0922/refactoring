aba86968808abce13c5649397444d2c72e5aae4e
jooo0922 <2014102458@khu.ac.kr>
2022-07-30 18:23:52 +0900

    refactor: 문장 슬라이드 및 추출한 코드를 새 함수로 복사

--- ch6/6-1/app.js
+++ ch6/6-1/app.js
@@ -1,9 +1,8 @@
 function printOwing(invoice) {
-  let outstanding = 0;
-
   printBanner(); // 배너 출력 로직을 함수로 추출
 
   // 미해결 채무(outstanding)를 계산한다.
+  let outstanding = 0; // 맨 위에 있던 선언문을 이 위치로 이동 (문장 슬라이드하기. 연관된 코드들을 비슷한 위치에 모아두는 것!)
   for (const o of invoice.orders) {
     outstanding += o.amount;
   }
@@ -13,6 +12,15 @@ function printOwing(invoice) {
 
   // 세부사항을 출력한다.
   printDetails(invoice, outstanding); // 앞의 예와 달리 지역변수를 매개변수로 전달
+}
+
+function calculateOutstanding(invoice) {
+  // 추출할 코드 새 함수에 복사
+  let outstanding = 0;
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+  return outstanding; // 수정된 값 반환
 }
 
 function recordDueDate(invoice) {
