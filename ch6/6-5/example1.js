37fcbc7feea3f1fa95964abcc8d7362947de8592
jooo0922 <2014102458@khu.ac.kr>
2022-07-30 23:13:48 +0900

    add: 마이그레이션 절차를 따라 함수 이름 바꾸기 예제 작성

--- ch6/6-5/example1.js
+++ ch6/6-5/example1.js
@@ -1,4 +1,8 @@
 // 함수 이름을 너무 축약한 경우, 간단한 절차로 함수 선언문과 호출문을 모두 찾아 이름을 바꿈.
-function circumference(radius) {
+// function circumference(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+function circum(radius) {
   return 2 * Math.PI * radius;
 }
