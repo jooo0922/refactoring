8c51de524e16833ac8ae5b77049c4ed836d77eb9
jooo0922 <2014102458@khu.ac.kr>
2022-07-30 23:28:03 +0900

    refactor: 새 함수 선언문과 호출문에 매개변수 추가

--- ch6/6-5/example2.js
+++ ch6/6-5/example2.js
@@ -4,10 +4,14 @@ class Book {
   }
 
   addReservation(customer) {
-    this.zz_addReservation(customer);
+    this.zz_addReservation(customer, false);
   }
 
-  zz_addReservation(customer) {
+  zz_addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
   }
 }
+
+const book = new Book();
+book.addReservation("john");
