c099096b52011f0bfe72642fa859df1d1d1365e8
jooo0922 <2014102458@khu.ac.kr>
2022-07-30 23:41:52 +0900

    refactor: 임시 이름을 붙여 함수 추출

--- ch6/6-5/example3.js
+++ ch6/6-5/example3.js
@@ -21,6 +21,11 @@ const somCustomers = [
 
 function inNewEngland(aCustomer) {
   const stateCode = aCustomer.address.state; // 매개변수로 사용할 코드를 변수로 추출
+  return xxNEWinNewEngland(stateCode);
+}
+
+// 임시 이름을 붙여 함수 추출
+function xxNEWinNewEngland(stateCode) {
   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
