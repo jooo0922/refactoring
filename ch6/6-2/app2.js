fa7ea0ee89924d17432491746265c24d6be9d8cd
jooo0922 <2014102458@khu.ac.kr>
2022-07-30 21:26:28 +0900

    refactor: 여러 문장을 호출한 곳으로 옮겨서 함수 인라인하기

--- ch6/6-2/app2.js
+++ ch6/6-2/app2.js
@@ -1,10 +1,7 @@
 function reportLines(aCustomer) {
   const lines = [];
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]); // 여러 문장을 호출한 곳으로 옮기기
   gatherCustomerData(lines, aCustomer);
   return lines;
-}
-
-function gatherCustomerData(out, aCustomer) {
-  out.push(["name", aCustomer.name]);
-  out.push(["location", aCustomer.location]);
 }
