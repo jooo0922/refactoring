2e6a6fd1bebe7c10d976ee50d8680696bfa5ff44
jooo0922 <2014102458@khu.ac.kr>
2022-08-02 01:59:08 +0900

    refactor: 변수 캡슐화 후 변수 이름 변경

--- ch6/6-7/example1.js
+++ ch6/6-7/example1.js
@@ -3,7 +3,7 @@ const obj = {
 };
 
 let result = "";
-let tpHd = "untitled";
+let _title = "untitled";
 
 result += `<h1>${title()}</h1>`; // 전역변수 참조
 
@@ -12,9 +12,9 @@ setTitle(obj["articleTitle"]); // 전역변수 수정
 // 전역변수처럼 유효범위가 넓은 변수들은 이름을 바꾸기 전 접근자 메서드로 캡슐화함
 // tpHd 변수의 게터
 function title() {
-  return tpHd;
+  return _title;
 }
 // tpHd 변수의 세터
 function setTitle(arg) {
-  tpHd = arg;
+  _title = arg;
 }
