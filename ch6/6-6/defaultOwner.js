45685ab793a693c9bf909561206e003c44537d99
jooo0922 <2014102458@khu.ac.kr>
2022-08-02 01:38:57 +0900

    refactor: 게터에서 데이터를 복사하여 리턴

--- ch6/6-6/defaultOwner.js
+++ ch6/6-6/defaultOwner.js
@@ -5,7 +5,8 @@ let defaultOwnerData = { firstName: "마틴", lastName: "파울러" }; // 중요
 // 캡슐화를 위해 데이터를 읽고 쓰는 함수(getter, setter) 를 정의함.
 // js 에서 getter 이름에는 get을 빼고, setter 이름에만 set 을 붙여서 구분하는게 좋다고 함.
 export function defaultOwner() {
-  return defaultOwnerData;
+  // 게터에서 데이터를 복사하여 리턴함으로써, 원본데이터 훼손을 방지함.
+  return Object.assign({}, defaultOwnerData);
 }
 export function setDefaultOwner(arg) {
   defaultOwnerData = arg;
