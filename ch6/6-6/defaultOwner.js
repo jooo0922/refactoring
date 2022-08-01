29409060dc46dea9328f22ef0b8df81ff9f7ef73
jooo0922 <2014102458@khu.ac.kr>
2022-08-02 01:30:11 +0900

    refactor: 전역변수 및 접근자 메서드를 모아서 파일로 추출

---
+++ ch6/6-6/defaultOwner.js
@@ -0,0 +1,12 @@
+// js 에서 특정 변수의 접근제한자를 설정하려면, 변수와 접근자만 따로 파일로 빼낸 뒤, 접근자만 exort 시켜서 사용하면 됨.
+
+let defaultOwnerData = { firstName: "마틴", lastName: "파울러" }; // 중요한 데이터가 담긴 전역변수
+
+// 캡슐화를 위해 데이터를 읽고 쓰는 함수(getter, setter) 를 정의함.
+// js 에서 getter 이름에는 get을 빼고, setter 이름에만 set 을 붙여서 구분하는게 좋다고 함.
+export function defaultOwner() {
+  return defaultOwnerData;
+}
+export function setDefaultOwner(arg) {
+  defaultOwnerData = arg;
+}
--- ch6/6-6/example1.js
+++ ch6/6-6/example1.js
@@ -1,3 +1,5 @@
+import { getDefaultOwner, setDefaultOwner } from "./defaultOwner";
+
 const spaceship = {
   owner: {
     firstName: null,
@@ -5,15 +7,5 @@ const spaceship = {
   },
 };
 
-let defaultOwner = { firstName: "마틴", lastName: "파울러" }; // 중요한 데이터가 담긴 전역변수
-
-// 캡슐화를 위해 데이터를 읽고 쓰는 함수(getter, setter) 를 정의함.
-function getDefaultOwner() {
-  return defaultOwner;
-}
-function setDefaultOwner(arg) {
-  defaultOwner = arg;
-}
-
 spaceship.owner = getDefaultOwner(); // 전역변수 참조
-defaultOwner = setDefaultOwner({ firstName: "레베카", lastName: "파슨스" }); // 전역변수 갱신
+setDefaultOwner({ firstName: "레베카", lastName: "파슨스" }); // 전역변수 갱신
