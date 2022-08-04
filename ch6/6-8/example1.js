14898983d6351db9b7c37320db48a857a413b0b2
jooo0922 <2014102458@khu.ac.kr>
2022-08-05 00:51:14 +0900

    refactor: 매개변수 객체 클래스 생성

--- ch6/6-8/example1.js
+++ ch6/6-8/example1.js
@@ -9,6 +9,21 @@ const station = {
   ],
 };
 
+class NumberRange {
+  constructor(min, max) {
+    this._data = {
+      min: min,
+      max: max,
+    };
+  }
+  get min() {
+    return this._data.min;
+  }
+  get max() {
+    return this._data.max;
+  }
+}
+
 // 정상 범위를 벗어난 측정값을 찾는 함수
 function readingsOutsideRange(station, min, max) {
   return station.readings.filter((r) => r.temp < min || r.temp > max);
