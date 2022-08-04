1caf9c846394c8fd58a93cde494413c327d1d395
jooo0922 <2014102458@khu.ac.kr>
2022-08-05 01:55:57 +0900

    refactor: 레코드를 클래스로 변환하기 위해 레코드 캡슐화

--- ch6/6-9/example1.js
+++ ch6/6-9/example1.js
@@ -17,6 +17,27 @@ function taxThreshold(year) {
   return 1;
 }
 
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() {
+    return this._customer;
+  }
+  get quantity() {
+    return this._quantity;
+  }
+  get month() {
+    return this._month;
+  }
+  get year() {
+    return this._year;
+  }
+}
+
 const aReading = acquireReading();
 const base = calculateBaseCharge(aReading);
 const texableCharge = Math.max(0, base - taxThreshold(aReading.year));
