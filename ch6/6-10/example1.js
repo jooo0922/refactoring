fa4f97816a835d32a0659edd7c458f71046b12be
jooo0922 <2014102458@khu.ac.kr>
2022-08-06 17:11:56 +0900

    add: 여러 함수를 변환함수로 묶기 예제 작성

---
+++ ch6/6-10/example1.js
@@ -0,0 +1,51 @@
+function acquireReading() {
+  return {
+    customer: "ivan",
+    quantity: 10,
+    month: 5,
+    year: 2017,
+  };
+}
+
+function baseRate(month, year) {
+  // 임시로 작성한 함수
+  return 0.5;
+}
+
+function taxThreshold(year) {
+  // 임시로 작성한 함수
+  return 1;
+}
+
+// 재귀함수를 이용해 객체를 깊은 복사하여 반환하는 함수
+function cloneDeep(obj) {
+  const result = {};
+
+  for (let key in obj) {
+    if (typeof obj[key] === "object") {
+      result[key] = copyObj(obj[key]);
+    } else {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
+}
+
+// 클라이언트 1...
+{
+  const aReading = acquireReading();
+  const baseCharge =
+    baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+// 클라이언트 3...
+{
+  const aReading = acquireReading();
+  const basicChargeAmount = calculateBaseCharge(aReading);
+
+  function calculateBaseCharge(aReading) {
+    // 프로그램 곳곳에서 중복된 로직들이 사용되고 있음.
+    return baseRate(aReading.month, aReading.year) * aReading.quantity;
+  }
+}
