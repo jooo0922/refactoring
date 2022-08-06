0b7fc94a716f9d0e0a6887f7e47e89619d95f76e
jooo0922 <2014102458@khu.ac.kr>
2022-08-06 17:40:01 +0900

    refactor: 클라이언트 2 도 부가정보가 담긴 필드를 사용하도록 수정

--- ch6/6-10/example1.js
+++ ch6/6-10/example1.js
@@ -41,8 +41,9 @@ function cloneDeep(obj) {
 
 // 클라이언트 2...
 {
-  const aReading = acquireReading();
-  const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading); // 미가공 측정값을 계산로직에 전달하기 전, 부가정보를 덧붙이도록 수정함
+  const base = aReading.baseCharge; // 클라이언트 2도 부가정보가 담긴 필드를 사용하도록 수정함
   const texableCharge = Math.max(0, base - taxThreshold(aReading.year));
 }
 
