39a71dd145680bdeda3bd1d0a0dfea9f3b9f88c4
jooo0922 <2014102458@khu.ac.kr>
2022-08-06 17:31:18 +0900

    refactor: 미가공 측정값에 부가정보를 덧붙여 클라이언트가 해당 정보가 담긴 필드를 사용하도록 수정

--- ch6/6-10/example1.js
+++ ch6/6-10/example1.js
@@ -41,17 +41,19 @@ function cloneDeep(obj) {
 
 // 클라이언트 3...
 {
-  const aReading = acquireReading();
-  const basicChargeAmount = calculateBaseCharge(aReading);
+  const rawReading = acquireReading();
+  const aReading = enrichReading(rawReading); // 미가공 측정값을 계산로직에 전달하기 전, 부가정보를 덧붙이도록 수정함
+  const basicChargeAmount = aReading.baseCharge; // 클라이언트가 부가정보가 담긴 필드를 사용하도록 수정함
+}
 
-  function calculateBaseCharge(aReading) {
-    // 프로그램 곳곳에서 중복된 로직들이 사용되고 있음.
-    return baseRate(aReading.month, aReading.year) * aReading.quantity;
-  }
+function calculateBaseCharge(aReading) {
+  // 프로그램 곳곳에서 중복된 로직들이 사용되고 있음.
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
 }
 
 // 먼저 입력 객체를 그대로 깊은 복사하여 반환하는 변환함수 만듦.
 function enrichReading(original) {
   const result = cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result); // 계산로직의 출력값을 부가정보로 덧붙임
   return result;
 }
