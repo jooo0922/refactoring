5b0f9af1876381acb71bda6b2e9e5f9b80f6a4f1
jooo0922 <2014102458@khu.ac.kr>
2022-08-07 02:33:45 +0900

    refactor: 두 번째 단계 함수로 추출

--- ch6/6-11/example1.js
+++ ch6/6-11/example1.js
@@ -4,6 +4,12 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
+
+// 두 번째 단계(배송비 계산 코드) 함수로 추출
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
