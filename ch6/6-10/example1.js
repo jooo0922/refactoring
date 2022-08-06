cbceec6820068d4645444a95eeacd03b487187b0
jooo0922 <2014102458@khu.ac.kr>
2022-08-06 17:41:30 +0900

    refactor: base 변수를 인라인

--- ch6/6-10/example1.js
+++ ch6/6-10/example1.js
@@ -43,8 +43,10 @@ function cloneDeep(obj) {
 {
   const rawReading = acquireReading();
   const aReading = enrichReading(rawReading); // 미가공 측정값을 계산로직에 전달하기 전, 부가정보를 덧붙이도록 수정함
-  const base = aReading.baseCharge; // 클라이언트 2도 부가정보가 담긴 필드를 사용하도록 수정함
-  const texableCharge = Math.max(0, base - taxThreshold(aReading.year));
+  const texableCharge = Math.max(
+    0,
+    aReading.baseCharge - taxThreshold(aReading.year)
+  ); // base 변수 인라인
 }
 
 // 클라이언트 3...
