bf9d38b427cbb36a85d5837fddace6d10ac1f79e
jooo0922 <2014102458@khu.ac.kr>
2022-08-07 02:48:55 +0900

    refactor: 변수 인라인하기

--- ch6/6-11/example1.js
+++ ch6/6-11/example1.js
@@ -1,7 +1,8 @@
 function priceOrder(product, quantity, shippingMethod) {
   const priceData = calculatePricingData(product, quantity);
-  const price = applyShipping(priceData, shippingMethod); // 중간 데이터 구조를 두 번째 단계 함수의 인수로 추가
-  return price;
+  // 중간 데이터 구조를 두 번째 단계 함수의 인수로 추가
+  // 변수 인라인
+  return applyShipping(priceData, shippingMethod);
 }
 
 // 첫 번째 단계(상품 가격 계산 코드) 함수로 추출
@@ -12,12 +13,12 @@ function calculatePricingData(product, quantity) {
     product.basePrice *
     product.discountRate;
   // 첫 번째 단계에서 사용되는 매개변수들은 원본 훼손 방지를 위해 중간데이터 객체에 복사하여 두 번째 단계 함수로 전달함.
-  const priceData = {
+  // 변수 인라인
+  return {
     basePrice: basePrice,
     quantity: quantity,
     discount: discount,
-  }; // 중간 데이터 구조
-  return priceData; // 중간 데이터 구조 반환
+  }; // 중간 데이터 구조 반환
 }
 
 // 두 번째 단계(배송비 계산 코드) 함수로 추출
@@ -27,6 +28,5 @@ function applyShipping(priceData, shippingMethod) {
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - priceData.discount + shippingCost;
-  return price;
+  return priceData.basePrice - priceData.discount + shippingCost; // 변수 인라인
 }
