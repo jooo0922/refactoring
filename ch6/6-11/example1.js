22417fe9e80f5859fe3591ddb52d14e9186168e6
jooo0922 <2014102458@khu.ac.kr>
2022-08-07 02:43:49 +0900

    refactor: 중간 데이터 구조에 매개변수 추가

--- ch6/6-11/example1.js
+++ ch6/6-11/example1.js
@@ -4,30 +4,23 @@ function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = {}; // 중간 데이터 구조
-  const price = applyShipping(
-    priceData, // 중간 데이터 구조를 두 번째 단계 함수의 인수로 추가
-    basePrice,
-    shippingMethod,
-    quantity,
-    discount
-  );
+  // 첫 번째 단계에서 사용되는 매개변수들은 원본 훼손 방지를 위해 중간데이터 객체에 복사하여 두 번째 단계 함수로 전달함.
+  const priceData = {
+    basePrice: basePrice,
+    quantity: quantity,
+    discount: discount,
+  }; // 중간 데이터 구조
+  const price = applyShipping(priceData, shippingMethod); // 중간 데이터 구조를 두 번째 단계 함수의 인수로 추가
   return price;
 }
 
 // 두 번째 단계(배송비 계산 코드) 함수로 추출
-function applyShipping(
-  priceData,
-  basePrice,
-  shippingMethod,
-  quantity,
-  discount
-) {
+function applyShipping(priceData, shippingMethod) {
   const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
+    priceData.basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
       : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  const price = priceData.basePrice - priceData.discount + shippingCost;
   return price;
 }
