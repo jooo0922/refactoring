53eacfde31bc8c9953087e1b5917fe0c37389c51
jooo0922 <2014102458@khu.ac.kr>
2022-08-14 01:05:54 +0900

    refactor: 변수 캡슐화하기

--- ch7/7-1/example1.js
+++ ch7/7-1/example1.js
@@ -2,5 +2,10 @@ const organization = { name: "애크미 구스베리", country: "GB" };
 
 let result = "";
 const newName = "새로운 이름";
-result += `<h1>${organization.name}</h1>`; // 읽기 예
-organization.name = newName; // 쓰기 예
+result += `<h1>${getRawDataOfOrganization().name}</h1>`; // 읽기 예
+getRawDataOfOrganization().name = newName; // 쓰기 예
+
+// 변수 캡슐화
+function getRawDataOfOrganization() {
+  return organization;
+}
