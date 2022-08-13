de767f4503cf7da8cabb9e7d8856ee575f74d43a
jooo0922 <2014102458@khu.ac.kr>
2022-08-14 01:08:44 +0900

    refactor: 레코드를 클래스로 생성 및 교체

--- ch7/7-1/example1.js
+++ ch7/7-1/example1.js
@@ -1,4 +1,7 @@
-const organization = { name: "애크미 구스베리", country: "GB" };
+const organization = new Organization({
+  name: "애크미 구스베리",
+  country: "GB",
+}); // 레코드를 클래스로 교체
 
 let result = "";
 const newName = "새로운 이름";
@@ -7,5 +10,16 @@ getRawDataOfOrganization().name = newName; // 쓰기 예
 
 // 변수 캡슐화
 function getRawDataOfOrganization() {
+  return organization._data;
+}
+// 새 클래스의 인스턴스를 반환하는 함수 생성
+function getOrganization() {
   return organization;
+}
+
+// 레코드를 클래스로 생성
+class Organization {
+  constructor(data) {
+    this._data = data;
+  }
 }
