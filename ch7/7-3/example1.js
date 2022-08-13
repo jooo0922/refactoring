fd46898cda0b15c9a06c92580c3820f3c0dd6f2f
jooo0922 <2014102458@khu.ac.kr>
2022-08-14 03:32:51 +0900

    refactor: 필드 자가캡슐화

--- ch7/7-3/example1.js
+++ ch7/7-3/example1.js
@@ -1,7 +1,14 @@
 class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this._priority = data.priority;
     // 나머지 초기화 코드 생략
+  }
+  // 필드 캡슐화(자가 캡슐화)를 위한 접근자 지정
+  get priority() {
+    return this._priority;
+  }
+  set priority(aString) {
+    this._priority = aString;
   }
 }
 
