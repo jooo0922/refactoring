8f71b258b86cbf7671da03dab86974f40aac2249
jooo0922 <2014102458@khu.ac.kr>
2022-08-14 01:17:48 +0900

    refactor: 레코드 내부 필드들을 멤버변수로 펼침

--- ch7/7-1/example1.js
+++ ch7/7-1/example1.js
@@ -8,10 +8,6 @@ const newName = "새로운 이름";
 result += `<h1>${getOrganization().name}</h1>`; // 레코드 읽는 코드는 게터로 변경
 getOrganization().name = newName; // 레코드 갱신 코드는 세터로 변경
 
-// 변수 캡슐화
-function getRawDataOfOrganization() {
-  return organization._data;
-}
 // 새 클래스의 인스턴스를 반환하는 함수 생성
 function getOrganization() {
   return organization;
@@ -20,16 +16,26 @@ function getOrganization() {
 // 레코드를 클래스로 생성
 class Organization {
   constructor(data) {
-    this._data = data;
+    // 레코드 내의 필드들을 멤버변수로 펼쳐놓으면 더 깔끔
+    this._name = data.name;
+    this._country = data.country;
   }
 
   // 레코드 갱신 코드는 세터로 변경
   set name(aString) {
-    this._data.name = aString;
+    this._name = aString;
   }
 
   // 레코드 읽는 코드는 게터로 변경
   get name() {
-    return this._data.name;
+    return this._name;
+  }
+
+  set country(aCountryCode) {
+    this._country = aCountryCode;
+  }
+
+  get name() {
+    return this._country;
   }
 }
