7bb0412600a006e1781e3a556d0c72055a265bc1
jooo0922 <2014102458@khu.ac.kr>
2022-08-21 01:12:06 +0900

    refactor: 전화번호 관련 필드들을 새 클래스로 이동

--- ch7/7-5/example1.js
+++ ch7/7-5/example1.js
@@ -2,8 +2,6 @@ class Person {
   constructor() {
     this._telephoneNumber = new TelephoneNumber(); // 원래 클래스 생성자에 새로 추출한 클래스 인스턴스 생성
     this._name = "john";
-    this._officeAreaCode = "010";
-    this._officeNumber = "1234566";
   }
 
   get name() {
@@ -16,6 +14,28 @@ class Person {
     return `(${this.officeAreaCode}) ${this.officeNumber}`;
   }
   get officeAreaCode() {
+    return this._telephoneNumber.officeAreaCode;
+  }
+  set officeAreaCode(arg) {
+    this._telephoneNumber.officeAreaCode = arg;
+  }
+  get officeNumber() {
+    return this._telephoneNumber.officeNumber;
+  }
+  set officeNumber(arg) {
+    this._telephoneNumber.officeNumber = arg;
+  }
+}
+
+// 전화번호 관련 동작 및 데이터를 별도 클래스로 추출
+class TelephoneNumber {
+  constructor() {
+    // 전화번호 관련 필드들을 원래 클래스에서 새 클래스로 이동
+    this._officeAreaCode = "010";
+    this._officeNumber = "1234566";
+  }
+
+  get officeAreaCode() {
     return this._officeAreaCode;
   }
   set officeAreaCode(arg) {
@@ -27,9 +47,4 @@ class Person {
   set officeNumber(arg) {
     this._officeNumber = arg;
   }
-}
-
-// 전화번호 관련 동작 및 데이터를 별도 클래스로 추출
-class TelephoneNumber {
-  constructor() {}
 }
