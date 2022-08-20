f0f22b2871bae3908ce982ddea453dd7b9a93916
jooo0922 <2014102458@khu.ac.kr>
2022-08-21 01:37:24 +0900

    refactor: 타깃 클래스에 위임메서드 생성

--- ch7/7-6/example1.js
+++ ch7/7-6/example1.js
@@ -1,4 +1,4 @@
-// 배송 클래스 (소스 클래스)
+// 배송 클래스 (타깃 클래스)
 class Shipment {
   constructor() {
     this._trackingInformation = new TrackingInformation();
@@ -13,9 +13,12 @@ class Shipment {
   set trackingInformation(aTrackingInformation) {
     this._trackingInformation = aTrackingInformation;
   }
+  set shippingCompany(arg) {
+    this._trackingInformation.shippingCompany = arg;
+  } // 타깃 클래스에 위임 메서드 생성
 }
 
-// 배송 추적 정보 클래스 (타깃 클래스)
+// 배송 추적 정보 클래스 (소스 클래스)
 class TrackingInformation {
   constructor() {
     this._shippingCompany = "A택배";
@@ -38,3 +41,10 @@ class TrackingInformation {
     return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 }
+
+// 클라이언트
+const request = {
+  vendor: "B택배",
+};
+const aShipment = new Shipment();
+aShipment.shippingCompany = request.vendor; // 클라이언트 코드가 위임 메서드를 사용하도록 변경
