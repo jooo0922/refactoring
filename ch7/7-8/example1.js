184de75b7449eedd068aae0e64a68452ca9121da
jooo0922 <2014102458@khu.ac.kr>
2022-08-21 16:22:36 +0900

    add: 중개자 제거하기 예제 추가

---
+++ ch7/7-8/example1.js
@@ -0,0 +1,39 @@
+// 서버 객체
+class Person {
+  constructor(name) {
+    this._name = name;
+    this._department = new Department();
+  }
+  get name() {
+    return this._name;
+  }
+  // 위임 객체에서 데이터를 가져오는 위임메서드 생성
+  get manager() {
+    return this._department.manager;
+  }
+}
+
+// 위임 객체
+class Department {
+  constructor() {
+    this._chargeCode = "asf";
+    this._manager = "john";
+  }
+
+  get chargeCode() {
+    return this._chargeCode;
+  }
+  set chargeCode(arg) {
+    this._chargeCode = arg;
+  }
+  get manager() {
+    return this._manager;
+  }
+  set manager(arg) {
+    this._manager = arg;
+  }
+}
+
+// 클라이언트 코드
+const aPerson = new Person("jack");
+const manager = aPerson.manager; // 클라이언트에서 위임메서드를 사용하도록 수정
