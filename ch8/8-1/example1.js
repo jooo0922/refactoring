8c4a38d0d733bbaf465e8602f620acd78608a516
jooo0922 <2014102458@khu.ac.kr>
2022-09-10 17:21:34 +0900

    refactor: 소스함수가 타깃함수의 위임함수가 되도록 본문 수정

--- ch8/8-1/example1.js
+++ ch8/8-1/example1.js
@@ -10,30 +10,7 @@ function trackSummary(points) {
 
   // 총 거리 계산
   function calculateDistance() {
-    let result = 0;
-    for (let i = 0; i < points.length; i++) {
-      result += distance(points[i - 1], points[i]);
-    }
-    return result;
-
-    // 두 지점의 거리 계산
-    function distance(p1, p2) {
-      const EARTH_RADIUS = 3959; // 단위: 마일(mile)
-      const dLat = radians(p2.lat) - radians(p1.lat);
-      const dLon = radians(p2.lon) - radians(p1.lon);
-      const a =
-        Math.pow(Math.sin(dLat / 2), 2) +
-        Math.cos(radians(p2.lat)) +
-        Math.cos(radians(p1.lat)) +
-        Math.pow(Math.sin(dLon / 2), 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      return EARTH_RADIUS * c;
-    }
-
-    // 라디안 값으로 변환
-    function radians(degree) {
-      return (degree * Math.PI) / 180;
-    }
+    return top_calculateDistance(); // 소스함수가 타깃함수의 위임함수가 되도록 본문 수정
   }
 
   function calculateTime() {} // 총 시간 계산
