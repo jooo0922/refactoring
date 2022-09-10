1053906ea6b524c34df5d1d8a372d86bc68256d8
jooo0922 <2014102458@khu.ac.kr>
2022-09-11 00:40:48 +0900

    refactor: 겉보기 동작 체크 후 접근자 메서드 변경

--- ch8/8-2/example2.js
+++ ch8/8-2/example2.js
@@ -1,16 +1,12 @@
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    // 기존 Account 클래스에 매개변수로 받는 이자율과 AccountType 클래스의 이자율이 같은지 체크함.
-    // 이 값이 같아야 리팩토링이라고 할 수 있고, 이 값이 다르다면 리팩토링이라고 할 수 없음. 필드의 값이 변경되면 겉보기 동작이 달라지니까!
-    assert(interestRate === this._type.interestRate);
-    this._interestRate = interestRate;
   }
 
   // 필드 캡슐화는 이미 잘 되어있음.
   get interestRate() {
-    return this._interestRate;
+    return this._type.interestRate;
   }
 }
 
