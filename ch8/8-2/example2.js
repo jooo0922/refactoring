67403c50e1122005c5e833167b3060b99d8bee93
jooo0922 <2014102458@khu.ac.kr>
2022-09-11 00:29:37 +0900

    refactor: 타깃 클래스에 필드와 접근자 복사

--- ch8/8-2/example2.js
+++ ch8/8-2/example2.js
@@ -5,13 +5,19 @@ class Account {
     this._interestRate = interestRate;
   }
 
+  // 필드 캡슐화는 이미 잘 되어있음.
   get interestRate() {
     return this._interestRate;
   }
 }
 
 class AccountType {
-  constructor(nameString) {
+  constructor(nameString, interestRate) {
     this._name = nameString;
+    this._interestRate = interestRate;
+  }
+
+  get interestRate() {
+    return this._interestRate;
   }
 }
