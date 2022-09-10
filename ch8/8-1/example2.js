0bc7b5a8edec84c8329d503edb1bee90671fc59b
jooo0922 <2014102458@khu.ac.kr>
2022-09-10 18:03:24 +0900

    refactor: 소스 클래스 자체를 매개변수로 넘기도록 수정

--- ch8/8-1/example2.js
+++ ch8/8-1/example2.js
@@ -6,8 +6,7 @@ class Account {
   // 은행 이자 계산
   get backCharge() {
     let result = 4.5;
-    if (this.daysOverdrawn > 0)
-      result += this.type.overDraftCharge(this.daysOverdrawn); // 위임 메서드 인라인
+    if (this.daysOverdrawn > 0) result += this.type.overDraftCharge(this); // 소스컨텍스트에서 가져올 데이터가 많다면, 소스 클래스 자체를 매개변수로 넘길 수 있음.
     return result;
   }
 
@@ -23,13 +22,13 @@ class AccountType {
 
   // 초과 인출 이자 계산 메서드를 AccountType 으로 이동 -> 계좌타입이 프리미엄인지 아닌지에 따라 계산이 달라지므로, 계좌타입 클래스에 있는게 더 적합함.
   // daysOverDrawn() 메서드는 Accout 클래스에도 필요하므로, 매개변수로 값을 넘기도록 함.
-  overDraftCharge(daysOverdrawn) {
+  overDraftCharge(account) {
     if (this.isPremium) {
       const baseCharge = 10;
       if (this._daysOverdrawn <= 7) return baseCharge;
-      else return baseCharge + (daysOverdrawn - 7) * 0.85;
+      else return baseCharge + (account.daysOverDrawn - 7) * 0.85;
     } else {
-      return daysOverdrawn * 1.75;
+      return account.daysOverDrawn * 1.75;
     }
   }
 }
