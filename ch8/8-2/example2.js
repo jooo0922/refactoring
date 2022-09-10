af579a7f189d4913c04e8f02e9417781677bece7
jooo0922 <2014102458@khu.ac.kr>
2022-09-11 00:39:30 +0900

    refactor: 어서션을 추가하여 수정 전후 겉보기 동작이 같은지 확인

--- ch8/8-2/example2.js
+++ ch8/8-2/example2.js
@@ -2,6 +2,9 @@ class Account {
   constructor(number, type, interestRate) {
     this._number = number;
     this._type = type;
+    // 기존 Account 클래스에 매개변수로 받는 이자율과 AccountType 클래스의 이자율이 같은지 체크함.
+    // 이 값이 같아야 리팩토링이라고 할 수 있고, 이 값이 다르다면 리팩토링이라고 할 수 없음. 필드의 값이 변경되면 겉보기 동작이 달라지니까!
+    assert(interestRate === this._type.interestRate);
     this._interestRate = interestRate;
   }
 
@@ -21,3 +24,6 @@ class AccountType {
     return this._interestRate;
   }
 }
+
+const accountType = new AccountType("가나계좌", 10.2);
+const account = new Account(10, accountType, 10.2);
