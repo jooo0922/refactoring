91afd41aa8e509ea7a1911ebe839065abf7ed05b
jooo0922 <2014102458@khu.ac.kr>
2022-09-10 17:08:36 +0900

    add: 중첩함수 최상위로 옮기기 예제 추가

---
+++ ch8/8-1/example1.js
@@ -0,0 +1,22 @@
+function trackSummary(points) {
+  const totalTime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+  return {
+    time: totalTime,
+    distance: totalDistance,
+    pace: pace,
+  };
+
+  // 총 거리 계산
+  function calculateDistance() {
+    let result = 0;
+    for (let i = 0; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+  }
+  function distance(p1, p2) {} // 두 지점의 거리 계산
+  function radians(degree) {} // 라디안 값으로 변환
+  function calculateTime() {} // 총 시간 계산
+}
