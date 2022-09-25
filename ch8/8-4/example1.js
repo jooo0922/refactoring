b8a9b91f3a8c8401082f527f1b7811334bbe9ab3
jooo0922 <2014102458@khu.ac.kr>
2022-09-26 01:25:04 +0900

    refactor: 남기고 싶은 코드만 새로운 함수로 추출

--- ch8/8-4/example1.js
+++ ch8/8-4/example1.js
@@ -15,9 +15,14 @@ function listRecentPhotos(outStream, photos) {
 }
 
 function emitPhotoData(outStream, photo) {
+  zztmp(outStream, photo);
+  outStream.write(`<p>위치: ${photo.location}</p>\n`);
+}
+
+function zztmp(outStream, photo) {
+  // 이동하지 않을 코드 (남기고 싶은 코드들만 새로운 함수로 추출한 것.)
   outStream.write(`<p>제목: ${photo.title}</p>\n`);
   outStream.write(`<p>날짜: ${photo.data.toDateString()}</p>\n`);
-  outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, photo) {
