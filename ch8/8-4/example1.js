114629b2df46efe2a41330a4ee74af5c92bfaaac
jooo0922 <2014102458@khu.ac.kr>
2022-09-26 01:30:40 +0900

    refactor: 원래 함수를 지워 인라인 마무리

--- ch8/8-4/example1.js
+++ ch8/8-4/example1.js
@@ -1,8 +1,8 @@
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
-  renderPhoto(outStream, photo);
-  zztmp(outStream, photo);
-  outStream.write(`<p>위치: ${photo.location}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  zztmp(outStream, person.photo);
+  outStream.write(`<p>위치: ${person.photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -10,16 +10,10 @@ function listRecentPhotos(outStream, photos) {
     .filter((p) => p.date > recentDataCutoff())
     .forEach((p) => {
       outStream.write("<div>\n");
-      zztmp(outStream, photo);
-      outStream.write(`<p>위치: ${photo.location}</p>\n`);
+      zztmp(outStream, p);
+      outStream.write(`<p>위치: ${p.location}</p>\n`);
       outStream.write("</div>\n");
     });
-}
-
-function zztmp(outStream, photo) {
-  // 이동하지 않을 코드 (남기고 싶은 코드들만 새로운 함수로 추출한 것.)
-  outStream.write(`<p>제목: ${photo.title}</p>\n`);
-  outStream.write(`<p>날짜: ${photo.data.toDateString()}</p>\n`);
 }
 
 function renderPhoto(outStream, photo) {
