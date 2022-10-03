1bd4afa3ab1334204d65e8a064004611fc60861a
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 23:16:48 +0900

    refactor: 각 반복문을 함수로 추출

--- ch8/8-7/example1.js
+++ ch8/8-7/example1.js
@@ -1,10 +1,17 @@
-let totalSalary = 0;
-for (const p of people) {
-  totalSalary += p.salary; // 부수효과가 있는 코드는 한쪽만 남기고 제거
-}
-let youngest = people[0] ? people[0].age : Infinity;
-for (const p of people) {
-  if (p.age < youngest) youngest = p.age; // 부수효과가 있는 코드는 한쪽만 남기고 제거
+return `최연소: ${youngestAge()}, 총 급여: ${totalSalary()}`;
+
+function youngestAge() {
+  let youngest = people[0] ? people[0].age : Infinity;
+  for (const p of people) {
+    if (p.age < youngest) youngest = p.age; // 부수효과가 있는 코드는 한쪽만 남기고 제거
+  }
+  return youngest;
 }
 
-return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
+function totalSalary() {
+  let totalSalary = 0;
+  for (const p of people) {
+    totalSalary += p.salary; // 부수효과가 있는 코드는 한쪽만 남기고 제거
+  }
+  return totalSalary;
+}
