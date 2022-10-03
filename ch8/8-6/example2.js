7c0d2458b5adce6680361edba5eacb28cb3d169b
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 22:49:54 +0900

    add: 조건문에서 슬라이드 예제 추가

---
+++ ch8/8-6/example2.js
@@ -0,0 +1,11 @@
+function getResult() {
+  let result;
+  if (availableResources.length === 0) {
+    result = createResource();
+    allocatedResources.push(result);
+  } else {
+    result = availableResources.pop();
+    allocatedResources.push(result);
+  }
+  return result;
+}
