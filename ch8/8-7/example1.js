4479286d813fb9f2b1a2fb50e08f9edf8f9e9098
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 23:21:10 +0900

    refactor: 반복문 -> 파이프라인으로 교체 및 알고리즘 교체

--- ch8/8-7/example1.js
+++ ch8/8-7/example1.js
@@ -1,17 +1,9 @@
 return `최연소: ${youngestAge()}, 총 급여: ${totalSalary()}`;
 
 function youngestAge() {
-  let youngest = people[0] ? people[0].age : Infinity;
-  for (const p of people) {
-    if (p.age < youngest) youngest = p.age; // 부수효과가 있는 코드는 한쪽만 남기고 제거
-  }
-  return youngest;
+  return Math.min(...people.map((p) => p.age)); // 반복문 -> 컬렉션 파이프라인으로 변경 -> 알고리즘 교체하기
 }
 
 function totalSalary() {
-  let totalSalary = 0;
-  for (const p of people) {
-    totalSalary += p.salary; // 부수효과가 있는 코드는 한쪽만 남기고 제거
-  }
-  return totalSalary;
+  return people.reduce((total, p) => total + p.salary, 0); // 반복문 -> 컬렉션 파이프라인으로 변경
 }
