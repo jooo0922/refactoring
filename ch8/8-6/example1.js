68c88324744ac318d20e2bd8f14208db8f9b81ec
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 22:45:13 +0900

    refactor: charge 변수 선언 및 할당문 슬라이드

--- ch8/8-6/example1.js
+++ ch8/8-6/example1.js
@@ -1,13 +1,13 @@
 const pricingPlan = retrievePricingPlan();
 const baseCharge = pricingPlan.base;
-let charge;
 const chargePerUnit = pricingPlan.unit;
 const order = retrieveOrder();
 const units = order.units; // order 변수 선언 및 할당 문장 슬라이드
-charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
 let discount; // 할인 관련 코드를 한데 모으기 위해 discount 변수 선언 슬라이드
 discount = discountableUnits * pricingPlan.discountFactor;
 if (order.isRepeat) discount += 20;
+let charge;
+charge = baseCharge + units * chargePerUnit; // charge 변수 선언 및 할당 문장 슬라이드
 charge = charge - discount;
 chargeOrder(charge);
