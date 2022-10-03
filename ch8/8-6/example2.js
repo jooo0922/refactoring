b48d65e305425ba4247e1a3b05e39417264fd042
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 22:51:19 +0900

    refactor: 중복된 문장을 조건문 밖으로 슬라이드

--- ch8/8-6/example2.js
+++ ch8/8-6/example2.js
@@ -2,10 +2,9 @@ function getResult() {
   let result;
   if (availableResources.length === 0) {
     result = createResource();
-    allocatedResources.push(result);
   } else {
     result = availableResources.pop();
-    allocatedResources.push(result);
   }
+  allocatedResources.push(result);
   return result;
 }
