5099a02ce6b343b6f7cf1e28c275334ddacbdea9
jooo0922 <2014102458@khu.ac.kr>
2022-10-04 00:22:53 +0900

    refactor: 결과 데이터를 .map() 파이프라인 연산으로 가공

--- ch8/8-8/example1.js
+++ ch8/8-8/example1.js
@@ -15,11 +15,12 @@ function acquireData(input) {
     .slice(1) // 첫줄을 건너뛰는 if 문을 .slice() 파이프라인으로 교체
     .filter((line) => line.trim() !== "") // 빈줄 건너뛰는 if 문을 filter() 파이프라인으로 교체
     .map((line) => line.split(",")) // 각 라인마다 csv 데이터 -> 문자열 배열로 변환하는 작업을 .map() 파이프라인으로 교체
-    .filter((record) => record[1].trim() === "India"); // 인도에 위치한 사무실 데이터 필터링을 .filter() 파이프라인으로 교체
+    .filter((record) => record[1].trim() === "India") // 인도에 위치한 사무실 데이터 필터링을 .filter() 파이프라인으로 교체
+    .map((record) => ({ city: record[0].trim(), phone: record[2].trim() })); // 결과 데이터를 .map() 파이프라인 연산으로 가공
   for (const line of loopItems) {
     const record = line;
     // .trim()을 왜 해주냐면, .csv 파일에서 "India, " 이런 식으로 각 항목이 저장되므로, 각 항목별 오른쪽 공백을 제거하기 위해서 trim() 을 쓰는것임.
-    result.push({ city: record[0].trim(), phone: record[2].trim() });
+    result.push(line);
   }
   return result;
 }
