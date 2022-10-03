3cde9ca59dc2d68480adcb202f7855357f21c850
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 23:13:48 +0900

    refactor: 부수효과가 있는 중복 제거

--- ch8/8-7/example1.js
+++ ch8/8-7/example1.js
@@ -1,12 +1,10 @@
 let youngest = people[0] ? people[0].age : Infinity;
 let totalSalary = 0;
 for (const p of people) {
-  if (p.age < youngest) youngest = p.age;
-  totalSalary += p.salary;
+  totalSalary += p.salary; // 부수효과가 있는 코드는 한쪽만 남기고 제거
 }
 for (const p of people) {
-  if (p.age < youngest) youngest = p.age;
-  totalSalary += p.salary;
+  if (p.age < youngest) youngest = p.age; // 부수효과가 있는 코드는 한쪽만 남기고 제거
 }
 
 return `최연소: ${youngest}, 총 급여: ${totalSalary}`;
