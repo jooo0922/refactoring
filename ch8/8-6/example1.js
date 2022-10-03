5d64ee319bfca3ff8e0719158c5e0af24331d2f3
jooo0922 <2014102458@khu.ac.kr>
2022-10-03 22:34:29 +0900

    refactor: discount 변수 선언 슬라이드

--- ch8/8-6/example1.js
+++ ch8/8-6/example1.js
@@ -4,9 +4,9 @@ const baseCharge = pricingPlan.base;
 let charge;
 const chargePerUnit = pricingPlan.unit;
 const units = order.units;
-let discount;
 charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - pricingPlan.discountThreshold, 0);
+let discount; // 할인 관련 코드를 한데 모으기 위해 discount 변수 선언 슬라이드
 discount = discountableUnits * pricingPlan.discountFactor;
 if (order.isRepeat) discount += 20;
 charge = charge - discount;
