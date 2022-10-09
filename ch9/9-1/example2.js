e3b098cacc420abacb3d165484469535cb1871cf
jooo0922 <2014102458@khu.ac.kr>
2022-10-09 21:55:50 +0900

    refactor: 쪼개진 두 변수를 각 용도에 적합한 이름으로 변경

--- ch9/9-1/example2.js
+++ ch9/9-1/example2.js
@@ -1,6 +1,6 @@
-function discount(originInputValue, quantity) {
-  let inputValue = originInputValue; // 1. 함수에 데이터를 전달하는 용도, 2. 결과값을 반환하는 용도 로 매개변수를 쪼개줌
-  if (inputValue > 50) inputValue = inputValue - 2;
-  if (quantity > 100) inputValue = inputValue - 1;
-  return inputValue;
+function discount(inputValue, quantity) {
+  let result = inputValue; // 1. 함수에 데이터를 전달하는 용도, 2. 결과값을 반환하는 용도 로 매개변수를 쪼개줌
+  if (inputValue > 50) result = result - 2;
+  if (quantity > 100) result = result - 1;
+  return result;
 }
