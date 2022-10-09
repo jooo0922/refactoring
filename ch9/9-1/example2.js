a028efec0b1bc0b3ae93e26e8fcea8bd3d3487bd
jooo0922 <2014102458@khu.ac.kr>
2022-10-09 21:54:35 +0900

    refactor: 입력 매개변수를 용도에 맞게 쪼개기

--- ch9/9-1/example2.js
+++ ch9/9-1/example2.js
@@ -1,4 +1,5 @@
-function discount(inputValue, quantity) {
+function discount(originInputValue, quantity) {
+  let inputValue = originInputValue; // 1. 함수에 데이터를 전달하는 용도, 2. 결과값을 반환하는 용도 로 매개변수를 쪼개줌
   if (inputValue > 50) inputValue = inputValue - 2;
   if (quantity > 100) inputValue = inputValue - 1;
   return inputValue;
