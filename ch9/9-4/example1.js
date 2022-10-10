702cd96cdb7d63f12bd4efc5a78268d7047de801
jooo0922 <2014102458@khu.ac.kr>
2022-10-11 00:37:26 +0900

    refactor: 세터 제거로 데이터를 불변으로 변경

--- ch9/9-4/example1.js
+++ ch9/9-4/example1.js
@@ -18,20 +18,14 @@ class Person {
 }
 
 class TelephoneNumber {
-  constructor() {
-    this._areaCode = "010";
-    this._number = "01012345678";
+  constructor(areaCode, number) {
+    this._areaCode = areaCode;
+    this._number = number;
   }
   get areaCode() {
     return this._areaCode;
   }
-  set areaCode(arg) {
-    this._areaCode = arg;
-  }
   get number() {
     return this._number;
-  }
-  set number(arg) {
-    this._number = arg;
   }
 }
