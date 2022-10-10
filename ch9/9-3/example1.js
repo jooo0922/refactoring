d1a9b30e7b9cd973fe17eeb7bbef08236d3dd11a
jooo0922 <2014102458@khu.ac.kr>
2022-10-10 23:57:32 +0900

    refactor: production 가변데이터 누적계산 대체 로직 추가 후, assert() 로 검증

--- ch9/9-3/example1.js
+++ ch9/9-3/example1.js
@@ -5,7 +5,11 @@ class ProductionPlan {
   }
 
   get production() {
+    assert(this._production === this.calculatedProduction);
     return this._production;
+  }
+  get calculatedProduction() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0); // production 값 누적 계산을 질의함수로 변경
   }
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
