bb1beb4659b637d4d6a00af382e0360dba6c1db9
jooo0922 <2014102458@khu.ac.kr>
2022-10-11 00:00:17 +0900

    refactor: calculatedProduction 접근자 메서드 인라인

--- ch9/9-3/example1.js
+++ ch9/9-3/example1.js
@@ -5,9 +5,6 @@ class ProductionPlan {
   }
 
   get production() {
-    return this.calculatedProduction;
-  }
-  get calculatedProduction() {
     return this._adjustments.reduce((sum, a) => sum + a.amount, 0); // production 값 누적 계산을 질의함수로 변경
   }
   applyAdjustment(anAdjustment) {
