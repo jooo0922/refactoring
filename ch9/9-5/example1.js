39eb30fb6e8e1f1d4b917159de3f7ef7e28b1482
jooo0922 <2014102458@khu.ac.kr>
2022-10-16 23:54:14 +0900

    refactor: 유일한 고객데이터를 전역 저장소에 등록하도록 변경

--- ch9/9-5/example1.js
+++ ch9/9-5/example1.js
@@ -1,10 +1,11 @@
 import Customer from "./Customer";
+import { registerCustomer } from "./repository";
 
 // 주문목록 클래스
 class Order {
   constructor(data) {
     this._number = data.number;
-    this._customer = new Customer(data.customer); // data.customer 는 고객 ID -> 고객 ID 를 받아 주문목록 클래스마다 동일한 고객 클래스를 여러개 복사해서 생성하게 됨. -> 귀찮고 비효율적!
+    this._customer = registerCustomer(data.customer); // data.customer(고객 ID)를 입력받아 유일한 고객데이터를 저장소 객체에 등록 -> 동일한 Entity에 대하여 고객데이터 중복생성 방지
     // 다른 데이터를 읽어들인다.
   }
 
