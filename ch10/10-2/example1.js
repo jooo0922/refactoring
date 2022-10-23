675d42d9ac28d726f9182feac79f806ff1115cc3
jooo0922 <2014102458@khu.ac.kr>
2022-10-24 01:26:40 +0900

    refactor: 마지막 조건문도 논리연산자로 결합

--- ch10/10-2/example1.js
+++ ch10/10-2/example1.js
@@ -1,5 +1,9 @@
 function disablilityAmount(anEmployee) {
-  if (anEmployee.seniority < 2 || anEmployee.monthDisabled > 12) return 0;
-  if (anEmployee.isPartTime) return 0;
+  if (
+    anEmployee.seniority < 2 ||
+    anEmployee.monthDisabled > 12 ||
+    anEmployee.isPartTime
+  )
+    return 0;
   // 장애 수당 계산
 }
