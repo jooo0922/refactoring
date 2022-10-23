dc9ec020da767f4978121d222ded015b4d2bf8b9
jooo0922 <2014102458@khu.ac.kr>
2022-10-24 01:27:45 +0900

    refactor: 최종 조건식을 함수로 추출

--- ch10/10-2/example1.js
+++ ch10/10-2/example1.js
@@ -1,9 +1,12 @@
 function disablilityAmount(anEmployee) {
-  if (
+  if (isNotEligibleForDisability(anEmployee)) return 0;
+  // 장애 수당 계산
+}
+function isNotEligibleForDisability(anEmployee) {
+  // 장애 수단 적용여부 확인
+  return (
     anEmployee.seniority < 2 ||
     anEmployee.monthDisabled > 12 ||
     anEmployee.isPartTime
-  )
-    return 0;
-  // 장애 수당 계산
+  );
 }
