2a0fea0d65e0bf36bfc42fffb1e981aa8cfb80bd
jooo0922 <2014102458@khu.ac.kr>
2022-10-24 01:26:01 +0900

    refactor: 두 번째 조건문을 논리 연산자로 결합

--- ch10/10-2/example1.js
+++ ch10/10-2/example1.js
@@ -1,6 +1,5 @@
 function disablilityAmount(anEmployee) {
-  if (anEmployee.seniority < 2) return 0;
-  if (anEmployee.monthDisabled > 12) return 0;
+  if (anEmployee.seniority < 2 || anEmployee.monthDisabled > 12) return 0;
   if (anEmployee.isPartTime) return 0;
   // 장애 수당 계산
 }
