18aa9b55ea6cff6d2ecc62d18dbbff4c12cfb15f
jooo0922 <2014102458@khu.ac.kr>
2022-10-28 01:43:28 +0900

    refactor: result 변수 제거 후, 변수 쪼개기 처리

--- ch10/10-3/example2.js
+++ ch10/10-3/example2.js
@@ -1,13 +1,10 @@
 function adjustedCapital(anInstrument) {
-  let result = 0;
   if (
     anInstrument.capital <= 0 ||
     anInstrument.interestRate <= 0 ||
     anInstrument.duration <= 0
   )
-    return result;
-  result =
-    (anInstrument.income / anInstrument.duration) *
-    anInstrument.adjustmentFactor;
-  return result;
+    return 0;
+  return;
+  (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
