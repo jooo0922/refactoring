4ae2ffc366091855d8f867f233acfba1447d75db
jooo0922 <2014102458@khu.ac.kr>
2022-10-28 01:35:42 +0900

    refactor: 두 번째 조건문을 보호구문으로 변경

--- ch10/10-3/example1.js
+++ ch10/10-3/example1.js
@@ -1,12 +1,8 @@
 function payAmount(employee) {
   let result;
   if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" }; // 맨 바깥쪽 조건문을 보호구문으로 변경
-  if (employee.isRetired) {
-    // 은퇴한 직원인가?
-    result = { amount: 0, reasonCode: "RET" };
-  } else {
-    // 급여 계산 로직 -> 이 함수의 진짜 의도가 나타난 코드가 중첩 조건문 때문에 잘 보이지 않음.
-    result = someFinalComputation();
-  }
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  // 급여 계산 로직 -> 이 함수의 진짜 의도가 나타난 코드가 중첩 조건문 때문에 잘 보이지 않음.
+  result = someFinalComputation();
   return result;
 }
