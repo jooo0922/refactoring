868fd561cf2c163bd736f24a4141743faf4986bb
jooo0922 <2014102458@khu.ac.kr>
2022-11-09 01:00:36 +0900

    add: 클라이언트 코드 추가

--- ch10/10-4/example2.js
+++ ch10/10-4/example2.js
@@ -1,3 +1,13 @@
+// 클라이언트 코드
+const voyage = { zone: "서인도", length: 10 };
+const history = [
+  { zone: "동인도", profit: 5 },
+  { zone: "서인도", profit: 15 },
+  { zone: "중국", profit: -2 },
+  { zone: "서아프리카", profit: 7 },
+];
+const myRating = rating(voyage, history);
+
 // 투자 등급
 function rating(voyage, history) {
   const vpf = voyageProfitFactor(voyage, history);
