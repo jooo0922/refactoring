e25a4ed36a526a15ecb51ff9219b18e866493285
jooo0922 <2014102458@khu.ac.kr>
2022-11-08 22:13:29 +0900

    refactor: 조건부 로직이 포함된 함수를 슈퍼클래스로 묶기

--- ch10/10-4/example1.js
+++ ch10/10-4/example1.js
@@ -1,35 +1,41 @@
-function plumages(birds) {
-  return new Map(birds.map((b) => [b.name, plumage(b)]));
+function plumages(bird) {
+  return new Bird(bird).plumage;
 }
 
-function speeds(birds) {
-  return new Map(birds.map((b) => [b.name, airSpeedVelocity(b)]));
+function speeds(bird) {
+  return new Bird(bird).airSpeedVelocity;
 }
 
-// 깃털 상태 반환 함수
-function plumage(bird) {
-  switch (bird.type) {
-    case "유럽 제비":
-      return "보통이다";
-    case "아프리카 제비":
-      return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
-    case "노르웨이 파랑 앵무":
-      return bird.voltage > 100 ? "그을렸다" : "예쁘다";
-    default:
-      return "알 수 없다";
+class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
   }
-}
 
-// 비행 속도 반환 함수
-function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case "유럽 제비":
-      return 35;
-    case "아프리카 제비":
-      return 40 - 2 * bird.numberOfCoconuts;
-    case "노르웨이 파랑 앵무":
-      return bird.isNailed ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
+  // 깃털 상태 반환 게터
+  get plumage() {
+    switch (this.type) {
+      case "유럽 제비":
+        return "보통이다";
+      case "아프리카 제비":
+        return bird.numberOfCoconuts > 2 ? "지쳤다" : "보통이다";
+      case "노르웨이 파랑 앵무":
+        return bird.voltage > 100 ? "그을렸다" : "예쁘다";
+      default:
+        return "알 수 없다";
+    }
+  }
+
+  // 비행 속도 반환 함수
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case "유럽 제비":
+        return 35;
+      case "아프리카 제비":
+        return 40 - 2 * bird.numberOfCoconuts;
+      case "노르웨이 파랑 앵무":
+        return bird.isNailed ? 0 : 10 + bird.voltage / 10;
+      default:
+        return null;
+    }
   }
 }
