52989bc981e84724e0501930f84a1770cd907cbe
jooo0922 <2014102458@khu.ac.kr>
2022-11-23 01:24:01 +0900

    refactor: 어서션을 세터로 이동

--- ch10/10-6/example1.js
+++ ch10/10-6/example1.js
@@ -1,10 +1,8 @@
 const assert = require("assert");
-const customer = new Customer();
-customer.applyDiscount();
 
 class Customer {
   constructor() {
-    this._discountRate = 10;
+    this._discountRate = null;
   }
 
   get discountRate() {
@@ -12,14 +10,18 @@ class Customer {
   }
 
   set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0); // 세터에서 처음부터 값을 변경하는 시점부터 어서션으로 체크해야 어디서부터 처음 에러가 발생했는지 확인하기 수월함!
     this._discountRate = aNumber;
   }
 
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
     else {
-      assert(this.discountRate >= 0); // 터미널에서 node example1.js 실행하여 어서션이 에러메시지를 던지는지 확인해보기
       return aNumber - this.discountRate * aNumber;
     }
   }
 }
+
+const customer = new Customer();
+customer.discountRate = 10;
+customer.applyDiscount();
