b99fcfad9443d98c93ef8f7bacd46cea458e4650
jooo0922 <2014102458@khu.ac.kr>
2022-11-23 01:16:14 +0900

    refactor: 할인율 적용 로직에서 어서션 추가

--- ch10/10-6/example1.js
+++ ch10/10-6/example1.js
@@ -1,10 +1,25 @@
+const assert = require("assert");
+const customer = new Customer();
+customer.applyDiscount();
+
 class Customer {
   constructor() {
-    this.discountRate = 10;
+    this._discountRate = 10;
+  }
+
+  get discountRate() {
+    return this._discountRate;
+  }
+
+  set discountRate(aNumber) {
+    this._discountRate = aNumber;
   }
 
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
-    else return aNumber - this.discountRate * aNumber;
+    else {
+      assert(this.discountRate >= 0); // 터미널에서 node example1.js 실행하여 어서션이 에러메시지를 던지는지 확인해보기
+      return aNumber - this.discountRate * aNumber;
+    }
   }
 }
