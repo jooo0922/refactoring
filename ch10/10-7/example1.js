42383ab868d0eaa59806ca142b519873c3ac3fe1
jooo0922 <2014102458@khu.ac.kr>
2022-11-23 01:51:28 +0900

    refactor: 반복문을 배열 파이프라인으로 변경

--- ch10/10-7/example1.js
+++ ch10/10-7/example1.js
@@ -3,14 +3,6 @@ checkForMiscreants(people);
 // 생략
 
 function checkForMiscreants(people) {
-  for (const p of people) {
-    if (p === "조커") {
-      sendAlert();
-      return;
-    }
-    if (p === "사루만") {
-      sendAlert();
-      return;
-    }
-  }
+  // for...of 반복문을 배열 파이프라인 .some() 으로 변경
+  if (people.some((p) => ["조커", "사루만"].includes(p))) sendAlert();
 }
