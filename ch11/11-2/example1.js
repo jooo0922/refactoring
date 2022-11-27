f95b03cd9b2a60aa7d7c0308f56a4cd9db5fccad
jooo0922 <2014102458@khu.ac.kr>
2022-11-28 00:07:54 +0900

    refactor: 함수 내에서 리터럴 값을 매개변수로 대체

--- ch11/11-2/example1.js
+++ ch11/11-2/example1.js
@@ -13,7 +13,7 @@ function bottomBand(usage) {
 
 // 중간 범위에 해당하는 로직의 함수에서 사용량 상한선, 하한선 리터럴값을 매개변수화 함.
 function withinBand(usage, bottom, top) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0; // 100 보다 크면, 0 ~ 100 사이의 요금을, 100보다 작으면 0을 반환 -> 즉, 100 ~ 200 사이의 사용량에 대해서만 요금 반환
+  return usage > bottom ? Math.min(usage, top) - bottom : 0; // 100 보다 크면, 0 ~ 100 사이의 요금을, 100보다 작으면 0을 반환 -> 즉, 100 ~ 200 사이의 사용량에 대해서만 요금 반환
 }
 
 function topBand(usage) {
