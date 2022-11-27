29361c13b98c8c15d0500dd81463967d460b29e1
jooo0922 <2014102458@khu.ac.kr>
2022-11-28 01:06:06 +0900

    refactor: 플래그 인수 사용 함수를 불리언 값에 따라 각각의 래핑함수로 감싸서 호출

--- ch11/11-3/example2.js
+++ ch11/11-3/example2.js
@@ -4,6 +4,16 @@ aShipment.deliveryDate = deliveryDate(anOrder, true);
 // 배송일자 계산 호출 코드 2
 aShipment.deliveryDate = deliveryDate(anOrder, false);
 
+// 플래그 인수 사용 함수가 너무 복잡해서 손댈 엄둑가 안난다면, 불리언 값에 따라 각각 래핑함수로 감싸버림.
+function rushDeliveryData(anOrder) {
+  return deliveryDate(anOrder, true);
+}
+
+function regularDeliveryDate(anOrder) {
+  return deliveryDate(anOrder, false);
+}
+
+// 플래그 인수를 너무 복잡하게 사용하는 함수
 function deliveryDate(anOrder, isRush) {
   let result;
   let deliveryTime;
