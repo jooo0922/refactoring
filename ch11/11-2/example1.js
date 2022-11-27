89d3549ca3879430775639dfe9b721d58c3a9688
jooo0922 <2014102458@khu.ac.kr>
2022-11-28 00:07:19 +0900

    refactor: 중간 범위 함수에서 상한선, 하한선 리터럴값을 매개변수화

--- ch11/11-2/example1.js
+++ ch11/11-2/example1.js
@@ -1,7 +1,9 @@
 function baseCharge(usage) {
   if (usage > 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    bottomBand(usage) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    topBand(usage) * 0.07;
   return usd(amount);
 }
 
@@ -9,7 +11,8 @@ function bottomBand(usage) {
   return Math.min(usage, 100); // 0 ~ 100 사이의 요금 반환 -> 즉, 100 미만의 사용량에 대해서만 요금 반환
 }
 
-function middleBand(usage) {
+// 중간 범위에 해당하는 로직의 함수에서 사용량 상한선, 하한선 리터럴값을 매개변수화 함.
+function withinBand(usage, bottom, top) {
   return usage > 100 ? Math.min(usage, 200) - 100 : 0; // 100 보다 크면, 0 ~ 100 사이의 요금을, 100보다 작으면 0을 반환 -> 즉, 100 ~ 200 사이의 사용량에 대해서만 요금 반환
 }
 
