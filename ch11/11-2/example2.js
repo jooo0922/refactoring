57071fafe661f58c21e314df4bba132f6cc34016
jooo0922 <2014102458@khu.ac.kr>
2022-11-28 00:11:45 +0900

    feat: 함수 매개변수화 하기 기본 예제 추가

---
+++ ch11/11-2/example2.js
@@ -0,0 +1,8 @@
+// 로직은 동일하나 리터럴 값만 다른 두 함수
+function tenPercentRaise(aPerson) {
+  aPerson.salary = aPerson.salary.multiply(1.1);
+}
+
+function fivePercentRaise(aPerson) {
+  aPerson.salary = aPerson.salary.multiply(1.05);
+}
