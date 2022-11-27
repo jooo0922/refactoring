542de015cf2bfe78e6daa118363e5b47686debd8
jooo0922 <2014102458@khu.ac.kr>
2022-11-27 23:55:57 +0900

    refactor: 원래 함수에서 질의코드 제거

--- ch11/11-1/example1.js
+++ ch11/11-1/example1.js
@@ -19,12 +19,12 @@ function alertForMiscreant(people) {
   for (const p of people) {
     if (p === "조커") {
       setOffAlarms();
-      return "조커";
+      return;
     }
     if (p === "사루만") {
       setOffAlarms();
-      return "사루만";
+      return;
     }
   }
-  return "";
+  return;
 }
