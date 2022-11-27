f2a1bbd654e8167835cf054a33f0c00ef95e7718
jooo0922 <2014102458@khu.ac.kr>
2022-11-28 00:09:32 +0900

    refactor: 사용량 상한 호출 함수도 매개변수화 함수로 대체

--- ch11/11-2/example1.js
+++ ch11/11-2/example1.js
@@ -3,15 +3,11 @@ function baseCharge(usage) {
   const amount =
     withinBand(usage, 0, 100) * 0.03 +
     withinBand(usage, 100, 200) * 0.05 +
-    topBand(usage) * 0.07;
+    withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
 // 중간 범위에 해당하는 로직의 함수에서 사용량 상한선, 하한선 리터럴값을 매개변수화 함.
 function withinBand(usage, bottom, top) {
   return usage > bottom ? Math.min(usage, top) - bottom : 0; // 100 보다 크면, 0 ~ 100 사이의 요금을, 100보다 작으면 0을 반환 -> 즉, 100 ~ 200 사이의 사용량에 대해서만 요금 반환
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0; // 200보다 크면 200을 빼서 반환, 작으면 0을 반환 -> 즉, 200 이상의 사용량에 대해서만 요금 반환
 }
