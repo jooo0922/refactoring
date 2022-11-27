ffb6d989d2833aaba3bb7894e351e34cb0c5b0d0
jooo0922 <2014102458@khu.ac.kr>
2022-11-27 23:54:54 +0900

    refactor: 원래함수 -> 질의 함수로 변경

--- ch11/11-1/example1.js
+++ ch11/11-1/example1.js
@@ -1,5 +1,6 @@
 // 원래 함수 호출 코드
-const found = alertForMiscreant(people);
+const found = findMisreant(people); // 원래 함수 -> 질의 함수로 변경
+alertForMiscreant(people); // 원래 함수 호출 코드 복사
 
 // 원본함수 복사 후, 질의 목적에 맞게 반환값 변수의 이름을 참조하여 함수 이름 짓기
 function findMisreant(people) {
