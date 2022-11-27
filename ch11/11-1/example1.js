a8cbb68c76243fbdaa8ed12c4d86da69f59f43ec
jooo0922 <2014102458@khu.ac.kr>
2022-11-27 23:52:52 +0900

    refactor: 복사한 함수에서 부수효과 로직 제거

--- ch11/11-1/example1.js
+++ ch11/11-1/example1.js
@@ -2,11 +2,9 @@
 function findMisreant(people) {
   for (const p of people) {
     if (p === "조커") {
-      setOffAlarms();
       return "조커";
     }
     if (p === "사루만") {
-      setOffAlarms();
       return "사루만";
     }
   }
