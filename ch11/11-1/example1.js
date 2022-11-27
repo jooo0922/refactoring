20eff24bd734372550173e0c4f465eff2309d2cd
jooo0922 <2014102458@khu.ac.kr>
2022-11-27 23:52:26 +0900

    refactor: 원본함수 복사 후, 질의 목적에 맞게 함수 이름 짓기

--- ch11/11-1/example1.js
+++ ch11/11-1/example1.js
@@ -1,3 +1,18 @@
+// 원본함수 복사 후, 질의 목적에 맞게 반환값 변수의 이름을 참조하여 함수 이름 짓기
+function findMisreant(people) {
+  for (const p of people) {
+    if (p === "조커") {
+      setOffAlarms();
+      return "조커";
+    }
+    if (p === "사루만") {
+      setOffAlarms();
+      return "사루만";
+    }
+  }
+  return "";
+}
+
 function alertForMiscreant(people) {
   for (const p of people) {
     if (p === "조커") {
