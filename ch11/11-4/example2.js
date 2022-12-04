49e3115f9ccb02d59485f08da5c43f693ef32d6a
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:15:06 +0900

    refactor: HeatingPlan 클래스 안으로 새 함수 옮기기

--- ch11/11-4/example2.js
+++ ch11/11-4/example2.js
@@ -1,7 +1,7 @@
 // 호출자...
 const aPlan = new HeatingPlan();
 const tempRange = aRoom.daysTempRange;
-const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange) alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 class HeatingPlan {
@@ -10,11 +10,11 @@ class HeatingPlan {
       bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
     );
   }
-}
 
-function xxNEWwithinRange(aPlan, tempRange) {
-  const low = tempRange.low;
-  const high = tempRange.high;
-  const isWithinRange = aPlan.withinRange(low, high);
-  return isWithinRange;
+  xxNEWwithinRange(tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = this.withinRange(low, high);
+    return isWithinRange;
+  }
 }
