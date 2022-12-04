5589d7ca9905bf9f9cb0013393871702e1e181e2
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:20:23 +0900

    refactor: 호출자에서 추출한 변수 인라인

--- ch11/11-4/example2.js
+++ ch11/11-4/example2.js
@@ -1,7 +1,6 @@
 // 호출자...
 const aPlan2 = new HeatingPlan2();
-const tempRange = aRoom.daysTempRange;
-const isWithinRange = aPlan2.withinRange(tempRange);
+const isWithinRange = aPlan2.withinRange(aRoom.daysTempRange);
 if (!isWithinRange) alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 class HeatingPlan2 {
