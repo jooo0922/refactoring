a4f45dd2100bab0d1f971131fc1deab76f10db73
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:08:22 +0900

    refactor: 새 메서드 이름 변경

--- ch11/11-4/example1.js
+++ ch11/11-4/example1.js
@@ -1,10 +1,10 @@
 // 호출자...
 const aPlan = new HeatingPlan();
-if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
+if (!aPlan.withinRange(aRoom.daysTempRange))
   alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 class HeatingPlan {
-  xxNEWwithinRange(aNumberRange) {
+  withinRange(aNumberRange) {
     return (
       aNumberRange.bottom >= this._temperatureRange.low &&
       aNumberRange.top <= this._temperatureRange.high
