51580d3667643d66736d7e9212e7614d5478c008
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:16:18 +0900

    refactor: 원래 메서드를 새 메서드 안으로 인라인

--- ch11/11-4/example2.js
+++ ch11/11-4/example2.js
@@ -5,16 +5,12 @@ const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange) alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 class HeatingPlan {
-  withinRange(bottom, top) {
-    return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
-    );
-  }
-
   xxNEWwithinRange(tempRange) {
     const low = tempRange.low;
     const high = tempRange.high;
-    const isWithinRange = this.withinRange(low, high);
+    const isWithinRange =
+      tempRange.low >= this._temperatureRange.low &&
+      tempRange.high <= this._temperatureRange.high;
     return isWithinRange;
   }
 }
