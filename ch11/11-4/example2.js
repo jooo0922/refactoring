3be163f44193a283f109311eb586b40ca44b44c6
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:09:29 +0900

    refactor: 원래 메서드 호출 코드를 변수로 추출

--- ch11/11-4/example2.js
+++ ch11/11-4/example2.js
@@ -2,8 +2,8 @@
 const aPlan = new HeatingPlan();
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
-if (!aPlan.withinRange(low, high))
-  alerts.push("방 온도가 지정 범위를 벗어났습니다.");
+const isWithinRange = aPlan.withinRange(low, high);
+if (!isWithinRange) alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 class HeatingPlan {
   withinRange(bottom, top) {
