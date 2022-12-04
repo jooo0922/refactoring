e2b8a9395519a7cd5b8daf90baa4492ffcd2001b
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:04:32 +0900

    refactor: 매개변수들을 원하는 형태로 받는 빈 메서드 생성

--- ch11/11-4/example1.js
+++ ch11/11-4/example1.js
@@ -6,6 +6,8 @@ if (!aPlan.withinRange(low, high))
   alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 class HeatingPlan {
+  xxNEWwithinRange(aNumberRange) {}
+
   withinRange(bottom, top) {
     return (
       bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
