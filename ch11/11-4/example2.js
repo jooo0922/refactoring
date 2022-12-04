03485a7d157835d4d3df357c5e37a66fec14e619
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:10:17 +0900

    refactor: 입력 매개변수 추출

--- ch11/11-4/example2.js
+++ ch11/11-4/example2.js
@@ -1,7 +1,8 @@
 // 호출자...
 const aPlan = new HeatingPlan();
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
+const tempRange = aRoom.daysTempRange;
+const low = tempRange.low;
+const high = tempRange.high;
 const isWithinRange = aPlan.withinRange(low, high);
 if (!isWithinRange) alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
