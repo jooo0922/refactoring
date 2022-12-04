d2ce2832c1b947af44be9dcd0f4edafa035e0a87
jooo0922 <2014102458@khu.ac.kr>
2022-12-04 17:06:47 +0900

    refactor: 불필요한 변수 추출 코드 제거

--- ch11/11-4/example1.js
+++ ch11/11-4/example1.js
@@ -1,7 +1,5 @@
 // 호출자...
 const aPlan = new HeatingPlan();
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
 if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
   alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
