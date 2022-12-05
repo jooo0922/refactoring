1b3352bf5d1ddf5a4ab03240017641cc15aca695
jooo0922 <2014102458@khu.ac.kr>
2022-12-05 22:31:09 +0900

    refactor: 추출한 메서드 이름을 원래 이름으로 바꾸기

--- ch11/11-6/example1.js
+++ ch11/11-6/example1.js
@@ -1,7 +1,7 @@
 const thermostat = {}; // 전역객체
 
 class HeatingPlan {
-  xxNEWtargetTemperature(selectedTemperature) {
+  targetTemperature(selectedTemperature) {
     if (selectedTemperature > this._max) return _max;
     else if (selectedTemperature < this._min) return _min;
     else return selectedTemperature;
@@ -11,12 +11,12 @@ class HeatingPlan {
 // 호출자
 const thePlan = new HeatingPlan();
 if (
-  thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) >
+  thePlan.targetTemperature(thermostat.selectedTemperature) >
   thermostat.currentTemperature
 )
   setToHeat();
 else if (
-  thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) <
+  thePlan.targetTemperature(thermostat.selectedTemperature) <
   thermostat.currentTemperature
 )
   setToCool();
