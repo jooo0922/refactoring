6c38fc760e4d75d1be404ac3d9fc5a3d39b724ca
jooo0922 <2014102458@khu.ac.kr>
2022-12-05 22:14:54 +0900

    refactor: 매개변수 제거

--- ch11/11-5/example1.js
+++ ch11/11-5/example1.js
@@ -1,14 +1,14 @@
 class Order {
   get finalPrice() {
     const basePrice = this.quantity * this.itemPrice;
-    return this.discountedPrice(basePrice, this.discountLevel);
+    return this.discountedPrice(basePrice);
   }
 
   get discountLevel() {
     return this.quantity > 100 ? 2 : 1;
   }
 
-  discountedPrcie(basePrice, discountLevel) {
+  discountedPrcie(basePrice) {
     switch (this.discountLevel) {
       case 1:
         return basePrice * 0.95;
