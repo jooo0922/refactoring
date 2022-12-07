9156eca64560949fc3998a05c7cfb9a21d05af65
jooo0922 <2014102458@khu.ac.kr>
2022-12-08 01:15:32 +0900

    refactor: Engineer 인스턴스 전용 팩터리 함수 선언

--- ch11/11-8/example1.js
+++ ch11/11-8/example1.js
@@ -21,9 +21,13 @@ function createEmployee(name, typeCode) {
   return new Employee(name, typeCode);
 }
 
+function createEngineer(name) {
+  return new Employee(name, "E");
+}
+
 // 호출자1
 let candidate;
 candidate = createEmployee(document.name, document.empType);
 
 // 호출자2
-const leadEngineer = createEmployee(document.leadEngineer, "E");
+const leadEngineer = createEngineer(document.leadEngineer); // 함수에 문자열 리터럴을 인자로 전달하는 것은 '악취'에 해당하기 때문에, 해당 타입 전용 Employee 인스턴스를 생성하는 팩터리 함수를 추가로 선언함.
