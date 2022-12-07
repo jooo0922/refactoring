cc3c335ce92c91da7ad4592f8aee37259c59f413
jooo0922 <2014102458@khu.ac.kr>
2022-12-08 00:55:00 +0900

    feat: 세터 제거하기 예제 추가

---
+++ ch11/11-7/example1.js
@@ -0,0 +1,24 @@
+class Person {
+  constructor() {}
+
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    this._name = arg;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(arg) {
+    this._id = arg;
+  }
+}
+
+// 호출자
+const martin = new Person();
+martin.name = "마틴";
+martin.id = "1234";
