994dca52700b777b7a264e9ae64426482cf97628
jooo0922 <2014102458@khu.ac.kr>
2022-12-08 01:42:27 +0900

    refactor: 빈 클래스 생성 후, 해당 함수를 클래스로 이동

--- ch11/11-9/example1.js
+++ ch11/11-9/example1.js
@@ -1,18 +1,26 @@
+// 원래 함수는 전달함수 역할로 남겨두기
 function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+// 명령 객체
+class Scorer {
+  execute(candidate, medicalExam, scoringGuide) {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = "regular";
+    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+      certificationGrade = "low";
+      result -= 5;
+    }
+    // 비슷한 코드가 한참 이어짐
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  let certificationGrade = "regular";
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = "low";
-    result -= 5;
-  }
-  // 비슷한 코드가 한참 이어짐
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
