ca7212d168ab11658adf7c2ed02ded48638b3c1d
jooo0922 <2014102458@khu.ac.kr>
2022-12-08 01:45:05 +0900

    refactor: 명령이 받는 인수들을 생성자로 이동

--- ch11/11-9/example1.js
+++ ch11/11-9/example1.js
@@ -1,21 +1,29 @@
 // 원래 함수는 전달함수 역할로 남겨두기
 function score(candidate, medicalExam, scoringGuide) {
-  return new Scorer().execute(candidate, medicalExam, scoringGuide);
+  return new Scorer(candidate, medicalExam, scoringGuide).execute();
 }
 
 // 명령 객체
 class Scorer {
-  execute(candidate, medicalExam, scoringGuide) {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this._candidate = candidate;
+    this._medicalExam = medicalExam;
+    this._scoringGuide = scoringGuide;
+  }
+
+  execute() {
     let result = 0;
     let healthLevel = 0;
     let highMedicalRiskFlag = false;
 
-    if (medicalExam.isSmoker) {
+    if (this._medicalExam.isSmoker) {
       healthLevel += 10;
       highMedicalRiskFlag = true;
     }
     let certificationGrade = "regular";
-    if (scoringGuide.stateWithLowCertification(candidate.originState)) {
+    if (
+      this._scoringGuide.stateWithLowCertification(this._candidate.originState)
+    ) {
       certificationGrade = "low";
       result -= 5;
     }
