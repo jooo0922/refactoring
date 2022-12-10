ffe189e7479ee13e0b91c4296b94ed7a0f283d83
jooo0922 <2014102458@khu.ac.kr>
2022-12-10 22:11:59 +0900

    refactor: 불필요한 명령 클래스 제거

--- ch11/11-10/example1.js
+++ ch11/11-10/example1.js
@@ -1,14 +1,3 @@
-// 명령 객체
-class ChargeCalculator {
-  constructor(customer, usage, provider) {}
-
-  // 명령 실행 메서드
-  charge(customer, usage, provider) {
-    const baseCharge = customer.baseRate * usage;
-    return baseCharge + provider.connectionCharge;
-  }
-}
-
 // 호출자...
 monthCharge = charge(customElements, usage, provider);
 
