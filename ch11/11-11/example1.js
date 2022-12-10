98f1a532fd243499014eefeeb1c162e9a3eb201d
jooo0922 <2014102458@khu.ac.kr>
2022-12-10 22:17:47 +0900

    refactor: 반환값을 가리키는 변수 선언

--- ch11/11-11/example1.js
+++ ch11/11-11/example1.js
@@ -1,6 +1,7 @@
 let totalAscent = calculateAscent();
 
 function calculateAscent() {
+  let totalAscent;
   for (let i = 0; i < points.length; i++) {
     const verticalChange = points[i].elevation - points[i - 1].elevation;
     totalAscent += verticalChange > 0 ? verticalChange : 0;
