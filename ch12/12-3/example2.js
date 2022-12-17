3e6f80ef5dd20d4ce5bb083a4fb813a16a719d46
jooo0922 <2014102458@khu.ac.kr>
2022-12-17 23:42:02 +0900

    refactor: 공통 코드를 메서드로 추출

--- ch12/12-3/example2.js
+++ ch12/12-3/example2.js
@@ -14,7 +14,11 @@ class Manager extends Employee {
   constructor(name, grade) {
     super(name);
     this._grade = grade;
-    if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행
+    this.finishConstruction(); // 모든 서브클래스가 수행
+  }
+
+  finishConstruction() {
+    if (this.isPrivileged) this.assignCar();
   }
 
   get isPrivileged() {
