3eb417bfe1456bf6f9fc0e19d05e77622db06f80
jooo0922 <2014102458@khu.ac.kr>
2022-12-17 23:38:20 +0900

    feat: 공통 코드를 슈퍼클래스 생성자로 올리기 어려운 예제 추가

---
+++ ch12/12-3/example2.js
@@ -0,0 +1,23 @@
+class Employee {
+  constructor(name) {
+    // ...
+  }
+  get isPrivileged() {
+    // ...
+  }
+  assignCar() {
+    // ...
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, grade) {
+    super(name);
+    this._grade = grade;
+    if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행
+  }
+
+  get isPrivileged() {
+    return this._grade > 4;
+  }
+}
