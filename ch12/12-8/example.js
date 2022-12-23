bdde2103f60b4261e711d7d23e1e9c6ab2670790
jooo0922 <2014102458@khu.ac.kr>
2022-12-24 01:03:00 +0900

    refactor: 공통된 필드 슈퍼클래스로 올리기

--- ch12/12-8/example.js
+++ ch12/12-8/example.js
@@ -1,10 +1,13 @@
-class Party {}
+class Party {
+  constructor(name) {
+    this._name = name;
+  }
+}
 
 class Employee extends Party {
   constructor(name, id, monthlyCost) {
-    super();
+    super(name);
     this._id = id;
-    this._name = name;
     this._monthlyCost = monthlyCost;
   }
   // 월간비용
@@ -27,8 +30,7 @@ class Employee extends Party {
 
 class Department extends Party {
   constructor(name, staff) {
-    super();
-    this._name = name;
+    super(name);
     this._staff = staff;
   }
   get staff() {
