976f197c1a3d20cc17a780820368aabe7758e417
jooo0922 <2014102458@khu.ac.kr>
2022-12-26 23:35:52 +0900

    feat: 서브클래스가 여러 개일 때 위임으로 바꾸기 예제 추가

---
+++ ch12/12-10/example2.js
@@ -0,0 +1,65 @@
+// 팩터리 함수
+function createBird(data) {
+  switch (data.type) {
+    case "유럽 제비":
+      break;
+
+    default:
+      break;
+  }
+}
+
+// 슈퍼클래스
+class Bird {
+  constructor(data) {
+    this._name = data.name;
+    this._plumage = data.plumage;
+  }
+
+  get name() {
+    return this._name;
+  }
+  get plumage() {
+    return this._plumage || "보통이다";
+  }
+  get airSpeedVelocity() {
+    return null;
+  }
+}
+
+// 서브클래스1
+class EuropeanSwallow extends Bird {
+  get airSpeedVelocity() {
+    return 35;
+  }
+}
+
+// 서브클래스2
+class AfricanSwallow extends Bird {
+  constructor(data) {
+    super(data);
+    this._numberOfCoconuts = data._numberOfCoconuts;
+  }
+
+  get airSpeedVelocity() {
+    return 40 - 2 * this._numberOfCoconuts;
+  }
+}
+
+// 서브클래스3
+class NorwegianBlueParrot extends Bird {
+  constructor(data) {
+    super(data);
+    this._voltage = data.voltage;
+    this._isNailed = data._isNailed;
+  }
+
+  get plumage() {
+    if (this._voltage > 100) return "그을렸다";
+    else return this._plumage || "예쁘다";
+  }
+
+  get airSpeedVelocity() {
+    return this._isNailed ? 0 : 10 + this._voltage / 10;
+  }
+}
