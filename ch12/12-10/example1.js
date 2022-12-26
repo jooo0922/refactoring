dc2f6565b75151a62142899f369952fee3e79faf
jooo0922 <2014102458@khu.ac.kr>
2022-12-26 23:21:50 +0900

    refactor: 위임 메서드를 슈퍼클래스 기반 메서드의 확장 형태로 재호출하여 basePrice() 메서드 위임클래스로 이동

--- ch12/12-10/example1.js
+++ ch12/12-10/example1.js
@@ -13,7 +13,9 @@ class Booking {
   get basePrice() {
     let result = this._show.price;
     if (this.isPeakDay) result += Math.round(result * 0.15);
-    return result;
+    return this._premiumDelegate // 위임 존재 검사 보호코드로 감싸기
+      ? this._premiumDelegate.extendBasePrice(result)
+      : result;
   }
 
   _bePremium(extras) {
@@ -25,10 +27,6 @@ class PremiumBooking extends Booking {
   constructor(show, date, extras) {
     super(show, date);
     this._extras = extras;
-  }
-
-  get basePrice() {
-    return Math.round(super.basePrice + this._extras.premiumFee);
   }
 
   get hasDinner() {
@@ -45,6 +43,10 @@ class PremiumBookingDelegate {
   get hasTalkBack() {
     // 슈퍼클래스 데이터를 사용하기 위해 역참조 필드 _host 로 슈퍼클래스 역참조
     return this._host._show.hasOwnProperty("talkback");
+  }
+
+  extendBasePrice(base) {
+    return Math.round(base + this._extras.premiumFee); // 슈퍼클래스 기반 메서드의 확장 형태의 위임
   }
 }
 
